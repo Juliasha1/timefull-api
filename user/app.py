65ff104e77e09032275f3a183a380256681aabec
msjuliasha <msjuliasha@yandex.ru>
2025-04-10 16:55:42 +0300

    iteration_8

---
+++ user/app.py
@@ -0,0 +1,12 @@
+from config import create_app
+
+
+app = create_app()
+app.config['LOGIN_DISABLED'] = True
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+
+
+
+# flask db stamp head && flask db migrate && flask db upgrade   
