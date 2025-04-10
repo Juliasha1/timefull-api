9429fab5b1bb30b3c16611edf1dcd291b61d5f3a
msjuliasha <msjuliasha@yandex.ru>
2025-04-10 15:19:49 +0300

    iteration_7

--- user/models/user.py
+++ user/models/user.py
@@ -1,12 +1,15 @@
 from config.extensions import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(250))
     password = db.Column(db.String(250))
     phone = db.Column(db.String(250))
     sex = db.Column(db.String(250))
-    # packages = db.relationship('Packages')
+    name = db.Column(db.String(250))
+    name2 = db.Column(db.String(250))
+    age = db.Column(db.Integer)
     
 
     def serialize(self):
@@ -16,10 +19,17 @@ class User(db.Model):
             'password': self.password,
             'phone': self.phone,
             'sex': self.sex,
+            'name': self.name,
+            'name2': self.name2,
+            'age': self.age,
         }
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+    
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
 
     @classmethod
     def find_by_id(cls, id):
