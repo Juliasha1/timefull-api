377240c2a81319cc152a422e0784c980edba1594
msjuliasha <msjuliasha@yandex.ru>
2025-05-12 18:15:59 +0300

    - add note

--- modules/sport/api/index.py
+++ modules/sport/api/index.py
@@ -12,7 +12,7 @@ def _get():
             data,message = get(request.get_json())
             return response(data=data,message=message)
         else:
-            return response(data={},message=message1)
+            return response(data={'state':'not active package'},message=message1)
     except Exception as e:
         return ERROR(e)
     
@@ -24,9 +24,22 @@ def _add():
             data,message = add(request.get_json())
             return response(data=data,message=message)
         else:
-            return response(data={},message=message1)
+            return response(data={'state':'not active package'},message=message1)
     except Exception as e:
         return ERROR(e)
+    
+@swag_from('../swagger/save.yaml')
+def _save():
+    try:
+        cont,message1 = checkPackage('sport',request.get_json()['userId'])
+        if (cont):
+            data,message = save(request.get_json())
+            return response(data=data,message=message)
+        else:
+            return response(data={'state':'not active package'},message=message1)
+    except Exception as e:
+        return ERROR(e)
+
 @swag_from('../swagger/delete.yaml')
 def _delete():
     try:
@@ -35,7 +48,7 @@ def _delete():
             data,message = delete(request.get_json())
             return response(data=data,message=message)
         else:
-            return response(data={},message=message1)
+            return response(data={'state':'not active package'},message=message1)
     except Exception as e:
         return ERROR(e)
     
@@ -46,5 +59,6 @@ def _delete():
 sport_bp = Blueprint('sport_bp', __name__)
 sport_bp.add_url_rule('/sort/get',view_func=_get, methods=["POST"])
 sport_bp.add_url_rule('/sort/add',view_func=_add, methods=["POST"])
+sport_bp.add_url_rule('/sort/save',view_func=_save, methods=["POST"])
 sport_bp.add_url_rule('/sort/delete',view_func=_delete, methods=["DELETE"])
 
